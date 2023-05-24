5f635fe07eef8a6f4c7e0988faa8de3f7c674bd0
eduardocanova23 <64231485+eduardocanova23@users.noreply.github.com>
2023-05-23 23:00:16 -0300

    remove useless code

--- rectangleEditor1.py
+++ rectangleEditor1.py
@@ -11,30 +11,6 @@ def euclidean_distance(point1, point2):
     squared_distance = sum((p1 - p2) ** 2 for p1, p2 in zip(point1, point2))
     return math.sqrt(squared_distance)
 
-def calculate_angle(p1, p2, p3):
-    # Calculate the vectors between points
-    v1 = (p1[0] - p2[0], p1[1] - p2[1])
-    v2 = (p3[0] - p2[0], p3[1] - p2[1])
-
-    # Calculate the dot product
-    dot_product = v1[0] * v2[0] + v1[1] * v2[1]
-
-    # Calculate the magnitudes of the vectors
-    magnitude_v1 = math.sqrt(v1[0] ** 2 + v1[1] ** 2)
-    magnitude_v2 = math.sqrt(v2[0] ** 2 + v2[1] ** 2)
-
-    # Check for division by zero
-    if magnitude_v1 == 0 or magnitude_v2 == 0:
-        return 0.0  # Return 0 degrees if either magnitude is zero
-
-    # Calculate the angle between the vectors using the dot product formula
-    angle_radians = math.acos(max(-1, min(1, dot_product / (magnitude_v1 * magnitude_v2))))
-
-    # Convert the angle to degrees
-    angle_degrees = math.degrees(angle_radians)
-
-    return angle_degrees
-
 def apply_to_points(matrix, points):
     transformed_points = []
     for point in points:
@@ -91,49 +67,6 @@ class Circle:
 
         glPopMatrix()
 
-class Ellipse:
-    def __init__(self, center, radius_x, radius_y, m=create_identity()):
-        self.center = center
-        self.radius_x = radius_x
-        self.radius_y = radius_y
-        self.set_matrix(m)
-
-    def set_radii(self, radius_x, radius_y):
-        self.radius_x = radius_x
-        self.radius_y = radius_y
-
-    def set_matrix(self, t):
-        self.m = t
-        self.invm = inverse(t)
-
-    def contains(self, p):
-        p = apply_to_vector(self.invm, [p[0], p[1], 0, 1])
-        cx, cy = self.center
-        distance = ((p[0] - cx) ** 2) / (self.radius_x ** 2) + ((p[1] - cy) ** 2) / (self.radius_y ** 2)
-        return distance <= 1.0
-
-
-    def draw(self):
-        glPushMatrix()
-        glMultMatrixf(self.m)
-
-        glColor3f(0.4, 0.4, 0.4)
-        glTranslatef(self.center[0], self.center[1], 0)
-        gluDisk(gluNewQuadric(), 0, self.radius_x, 100, 1)
-        glTranslatef(-self.center[0], -self.center[1], 0)
-
-        glColor3f(1, 0, 1)
-        glLineWidth(2.0)
-        glBegin(GL_LINE_LOOP)
-        num_segments = 100
-        for i in range(num_segments):
-            theta = 2.0 * np.pi * i / num_segments
-            x = self.center[0] + self.radius_x * np.cos(theta)
-            y = self.center[1] + self.radius_y * np.sin(theta)
-            glVertex2f(x, y)
-        glEnd()
-
-        glPopMatrix()
 
 class Rect(object):
     def __init__ (self, points, m = create_identity()):
@@ -173,15 +106,11 @@ class Rect(object):
         glPopMatrix()
 
 picked = None
-modeConstants = ["CREATE", "TRANSLATE", "ROTATE", "RESHAPE"]
-shapeConstants = ["RECTANGLE", "CIRCLE"]
+modeConstants = ["CREATE RECTANGLE", "CREATE CIRCLE", "TRANSLATE", "ROTATE", "RESHAPE"]
 mode = modeConstants[0]
-shape = shapeConstants[0]
 current_mouse_pos = [0,0]
 last_angle = 0
 angle = 0
-
-
 
 def mouse (button, state, x, y):
     global lastx,lasty,picked,lastx_whenclicked,lasty_whenclicked,angle, last_angle
@@ -190,12 +119,12 @@ def mouse (button, state, x, y):
         print(lastx_whenclicked,lasty_whenclicked)
 
     if state!=GLUT_DOWN: return
-    if mode == "CREATE":
-        glutSetMenu(shape_menu_id)
-        if shape == "RECTANGLE":
-            shapes.append(Rect([[x,y],[x,y]]))
-        elif shape == "CIRCLE":
-            shapes.append(Circle([x,y], 0))
+    if mode == "CREATE RECTANGLE":
+        shapes.append(Rect([[x,y],[x,y]]))
+
+    elif mode == "CREATE CIRCLE":
+        shapes.append(Circle([x,y], 0))
+
     elif mode == "TRANSLATE":
         picked = None
         for s in shapes:
@@ -222,25 +151,19 @@ def mouse_drag(x, y):
     global current_mouse_pos, last_mouse_pos, angle, last_angle, lastx, lasty
     last_mouse_pos = current_mouse_pos
     current_mouse_pos = [x, y]
-    if mode == "CREATE":
-        if shape == "RECTANGLE":
-            shapes[-1].set_point(1,[x,y])
-        elif shape == "CIRCLE":
-            shapes[-1].set_radius(euclidean_distance(shapes[-1].center,[x,y]))
+    if mode == "CREATE RECTANGLE":
+        
+        shapes[-1].set_point(1,[x,y])
+    elif mode == "CREATE CIRCLE":
+        shapes[-1].set_radius(euclidean_distance(shapes[-1].center,[x,y]))
+
     elif mode == "TRANSLATE":
         if picked:
             delta_x = x - lastx
             delta_y = y - lasty
-            #picked.set_point(0, [picked.points[0][0]+delta_x,picked.points[0][1]+delta_y])
-            #picked.set_point(1, [picked.points[1][0]+delta_x,picked.points[1][1]+delta_y])
             t = create_from_translation([x-lastx,y-lasty,0])
             picked.set_matrix(multiply(picked.m,t))
-            #transformed_points = apply_to_points(picked.m, picked.points)
-            #print(int(transformed_points[0][1]))
-            #picked.set_point(0,int(transformed_points[0][0]),int(transformed_points[0][1]))
-            #picked.set_point(1,int(transformed_points[1][0]),int(transformed_points[1][1]))
             lastx,lasty=x,y
-            #picked.updatePoints()
     elif mode == "ROTATE":
          if picked:
             center = picked.get_center()
@@ -258,35 +181,26 @@ def mouse_drag(x, y):
         delta_x = x - lastx
         delta_y = y - lasty
         
-        center = picked.get_center()
-
-        # Calculate scaling factors
+        if picked:
 
         
-        sx = 1 + delta_x / (2 * abs(center[0]-delta_x))
-        sy = 1 + delta_y / (2 * abs(center[1]-delta_y))
+            sx = 1 + delta_x / (2 * abs(center[0]-delta_x))
+            sy = 1 + delta_y / (2 * abs(center[1]-delta_y))
 
-        # Create the scaling matrix
-        S = np.array([[sx, 0, 0, 0],
+            S = np.array([[sx, 0, 0, 0],
                   [0, sy, 0, 0],
                   [0, 0, 1, 0],
                   [0, 0, 0, 1]])
 
-        # Apply matrix multiplication to update the picked shape's matrix
-        picked_matrix = picked.m
-        updated_matrix = np.dot(picked_matrix, S)
+            picked_matrix = picked.m
+            updated_matrix = np.dot(picked_matrix, S)
 
-        # Update the shape's matrix
-        picked.set_matrix(updated_matrix)
-        t = create_from_translation([-(x-lastx)/3,-(y-lasty)/3,0])
-        picked.set_matrix(multiply(picked.m,t))
-        lastx,lasty=x,y
+            picked.set_matrix(updated_matrix)
+            t = create_from_translation([-(x-lastx)/3,-(y-lasty)/3,0])
+            picked.set_matrix(multiply(picked.m,t))
+            lastx,lasty=x,y
     glutPostRedisplay()
 
-#print(f'x esquerda em cima: {self.points[0][0]}')
-#print(f'y esquerda em cima: {self.points[0][1]}')
-#print(f'x direita embaixo {self.points[1][0]}')
-#print(f'y direita embaixo {self.points[1][1]}')
 
 def reshape( width, height):
     glViewport(0,0,width,height)
@@ -318,20 +232,7 @@ def createMenu():
         glutAddMenuEntry(name, i)
 
     glutSetMenu(main_menu_id)
-    glutAddSubMenu("Shape Menu", shape_menu_id)
     glutAttachMenu(GLUT_RIGHT_BUTTON)
-
-def createShapeMenu():
-    global shape_menu_id
-    def doShapemenu(item):
-        global shape
-        shape = shapeConstants[item]
-        return 0
-
-    shape_menu_id = glutCreateMenu(doShapemenu)
-    for i, name in enumerate(shapeConstants):
-        glutAddMenuEntry(name, i)
-    #glutAttachMenu(GLUT_LEFT_BUTTON)
 
 
 glutInit(sys.argv);
@@ -342,7 +243,6 @@ glutMouseFunc(mouse)
 glutMotionFunc(mouse_drag)
 glutDisplayFunc(display); 
 glutReshapeFunc(reshape)
-createShapeMenu()
 createMenu()
 
 glutMainLoop();
